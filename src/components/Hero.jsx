cf2dd5d66b236a41a10058b48b4228cf2791f50a
Sariah Jackson <sarijah13@gmail.com>
2024-12-16 10:38:22 -0400

    add home page animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP);
 
 export default function Hero() {
+  useGSAP(()=>
+    {
+      const heroTimeline = gsap.timeline();
+
+      heroTimeline
+      .fromTo('.video',
+        {
+          scale: 0.1,
+          opacity: 0
+        },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          ease: 'power3.out'
+        }
+      )
+
+      .fromTo('.hero-text',
+        {
+          opacity:0,
+          y: 100
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          ease: 'power3.out'
+        }
+      )
+    }
+
+  )
   return (
     <section>
-        <div className='relative h-[95vh] lg:h-[100vh] w-full overflow-hidden'>
+        <div className='video relative h-[95vh] lg:h-[100vh] w-full overflow-hidden'>
             {/* Background video */}
-            <video autoPlay loop muted src="/drone-footage.mp4" className='absolute top-0 left-0 w-full h-full object-cover'></video>
+            <video id='video' autoPlay loop muted src="/drone-footage.mp4" className='absolute top-0 left-0 w-full h-full object-cover'></video>
             
             {/* Transparent overlay */}
-            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
+            <div className="video absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
             
-            <div className='absolute bottom-5 left-3 text-white lg:left-5'>
+            <div className='hero-text absolute bottom-5 left-3 text-white lg:left-5'>
                 <h1 className='italic lg:text-3xl'>visit the</h1>
                 <h1 className='uppercase text-6xl lg:text-8xl'>Big Little <br />Island</h1>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,15 +14,78 @@ import InformationCard from '../components/InformationCard';
 import { Link } from 'react-router-dom';
 import ScrollToTop from '../components/ScrollToTop';
 
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(useGSAP);
 
 
 export default function Home() {
+  useGSAP(()=>
+    {
+      gsap.fromTo('.text',
+        {
+          opacity: 0,
+          x: -100
+        },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 1,
+          ease: 'power3.out',
+          scrollTrigger: {
+            trigger: ".text", // Parent container that triggers the animation
+            start: "top 80%", // Starts when the top of the trigger is 80% down the viewport
+          },
+        }
+      )
+
+      gsap.fromTo('.card',
+        {
+          opacity: 0,
+          scale: 0.3
+        },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          stagger: 0.5,
+          ease: 'power3.inOut',
+          scrollTrigger: {
+            trigger: ".cards", // Parent container that triggers the animation
+            start: "top 80%", // Starts when the top of the trigger is 80% down the viewport
+          },
+        }
+      )
+
+      gsap.fromTo('.activity-imgs',
+        {
+          opacity: 0,
+          y: 100
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          stagger: 0.5,
+          ease: 'power3.inOut',
+          scrollTrigger: {
+            trigger: ".activity-imgs", // Parent container that triggers the animation
+            start: "top 80%", // Starts when the top of the trigger is 80% down the viewport
+          },
+        }
+      )
+    }
+  )
+
   return (
     <div className="flex flex-col gap-20">
       <ScrollToTop />
       <Hero />
 
-      <section className="px-5">
+      <section className="text px-5">
         <div className='lg:px-[15rem] mt-[5rem]'>
           <SectionHeading 
             subheading="The Island of your Dreams" 
@@ -55,8 +118,8 @@ export default function Home() {
         <SwiperSlide className="relative rounded-lg"><img className='h-full w-full absolute top-0 object-cover' src="/sunset.jpg" alt="sunset" /></SwiperSlide>
       </Swiper>
 
-      <section className='grid grid-cols-1 lg:grid-cols-2 px-8 lg:px-[15rem] lg:gap-5'>
-        <Link to='/travel'>
+      <section className='cards grid grid-cols-1 lg:grid-cols-2 px-8 lg:px-[15rem] lg:gap-5'>
+        <Link to='/travel' className='card'>
           <InformationCard
             icon={<FaPlaneArrival />}
             heading='Getting to Bequia'
@@ -64,7 +127,7 @@ export default function Home() {
           />
         </Link>
         
-        <Link to='/travel'>
+        <Link to='/travel' className='card'>
           <InformationCard
             icon={<IoMdBoat />}
             heading='Ferry Schedule'
@@ -72,30 +135,32 @@ export default function Home() {
           />
         </Link>
         
-        <InformationCard
-          icon={<FaCarAlt />}
-          heading='Getting around Bequia'
-          text='Rentals, taxis and more!'
-        />
+        <Link className='card'>
+          <InformationCard
+            icon={<FaCarAlt />}
+            heading='Getting around Bequia'
+            text='Rentals, taxis and more!'
+          />
+        </Link>
+        
 
-        <Link to='/accommodations'>
+        <Link to='/accommodations' className='card'>
           <InformationCard
             icon={<FaHotel />}
             heading='Where to Stay'
             text='All you need to know to get to the island'
           />
         </Link>
-        
       </section>
 
-      <section className='h-[100vh] bg-green px-5 py-12 lg:px-[15rem]'>
+      <section id='activities' className=' bg-green px-5 py-12 lg:px-[15rem]'>
         <SectionHeading 
           subheading='See and do'
           heading='Activities'
           headingColor="text-white"
         />
 
-        <div className='flex flex-col'>
+        <div className='activity-imgs flex flex-col'>
           <div className='h-52 w-full relative'>
             <img className='absolute top-0 left-0 w-full h-full object-cover' src="/activities/bequia-lightingup.jpg" alt="" />
           </div>
@@ -104,7 +169,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='flex flex-col gap-2 text-white font-dm py-5 lg:w-52'>
+        <div className='activity-imgs flex flex-col gap-2 text-white font-dm py-5 lg:w-52'>
           <Link to='/activities'>Places to Visit</Link>
           <Link to='/activities'>Festivals & Events</Link>
           <Link to='/activities'>Tours & Hiking</Link>
